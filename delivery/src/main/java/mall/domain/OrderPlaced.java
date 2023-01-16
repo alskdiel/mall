af8619aadd00b655aa33be4ca0d56db5e3c2ea80
HYEONJAE_CHO <alskdiel@gmail.com>
2023-01-16 17:11:51 +0900

    commit

---
+++ delivery/src/main/java/mall/domain/OrderPlaced.java
@@ -0,0 +1,20 @@
+package mall.domain;
+
+import mall.domain.*;
+import mall.infra.AbstractEvent;
+import lombok.*;
+import java.util.*;
+@Data
+@ToString
+public class OrderPlaced extends AbstractEvent {
+
+    private Long id;
+    private String customerId;
+    private String productName;
+    private Long productId;
+    private Integer qty;
+    private String status;
+    private String address;
+}
+
+
